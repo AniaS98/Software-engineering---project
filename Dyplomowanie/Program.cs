b32091bc44aab631a4f64101d89eaba1adf6dfe8
AniaS98 <44135167+AniaS98@users.noreply.github.com>
2021-01-21 00:29:22 +0100

    Verifing

---
+++ Dyplomowanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dyplomowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Zmiana");
+        }
+    }
+}
