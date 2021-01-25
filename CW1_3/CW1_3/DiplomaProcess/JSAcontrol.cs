37fd0256adc7a89a21abef371dbbb16fd5bfda8b
Kinga Kempa <kinga.kempa@bimodal-solutions.com>
2021-01-25 09:14:10 +0100

    state

---
+++ CW1_3/CW1_3/DiplomaProcess/JSAcontrol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW1_3.DiplomaProcess
+{
+    public class JSAcontrol : StageType
+    {
+        public string StageName = "Przeslanie pracy do JSA";
+
+        public override DateTime CalculateEndDate(DateTime data)
+        {
+            return data.AddDays(2);
+        }
+    }
+}
---
+++ CW1_3/CW1_3/DiplomaProcess/StageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW1_3.DiplomaProcess
+{
+    public abstract class StageType
+    {
+        public string StageName;
+        //public abstract void process(DiplomaProcess diplomaProcess);
+        //public abstract DateTime CalculateStartDate(DateTime date);
+        public abstract DateTime CalculateEndDate(DateTime date);
+
+
+    }
+}
